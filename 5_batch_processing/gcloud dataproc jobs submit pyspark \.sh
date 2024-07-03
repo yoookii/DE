4a72c78801a38280917c0d9182f6b5037c72e26d
yoookii <yoki.yutifan@gmail.com>
2024-07-03 14:15:41 -0700

    save job submit script

---
+++ 5_batch_processing/gcloud dataproc jobs submit pyspark \.sh
@@ -0,0 +1,10 @@
+gcloud dataproc jobs submit pyspark \
+  --project=inlaid-reactor-427616-m8 \
+  --cluster=de-cluster \
+  --region=us-central1 \
+  --jars=gs://spark-lib/bigquery/spark-3.5-bigquery-0.39.1.jar \
+  gs://mage-de-demo/code/06_spark_bigquery.py \
+  -- \
+    --input_green="gs://mage-de-demo/pq/green/2020/*/" \
+    --input_yellow="gs://mage-de-demo/pq/yellow/2020/*/" \
+    --output="trips_data_all.report-2020"
